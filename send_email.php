591b3d1e0e67b71f1a8b3ad52be75a64ed86696c
Kyle Hanson <khanson3@binghamton.edu>
2019-09-13 01:23:54 -0400

    Added injection checking
    I don't think it's necessary due to the email attribute being present but I added it for a just-in-case security check

--- send_email.php
+++ send_email.php
@@ -1,5 +1,13 @@
 <?php
 	$user_email = $_POST['email'];
+	
+	$injections = array('(\n+)', '(\r+)', '(\t+)', '(%0A+)', '(%0D+)', '(%08+)', '(%09+)');
+	
+	for($i = 0; $i < count($injections); $i++) {
+	    if(strpos($user_email, $injections[$i]) !== false) {
+	        exit;
+	    }
+	}
 
 	$my_email = "khanson3@binghamton.edu";
 	$subject = "Request for more info";
